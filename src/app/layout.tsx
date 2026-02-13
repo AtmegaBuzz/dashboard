c8c5b5f924a00677bdf41198c0cf8ff0884410cb
Swapnil Shinde <swapnilshinde9382@gmail.com>
2026-02-13 19:48:27 +0530

    fix css issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { clsx } from "clsx";
@@ -7,34 +8,28 @@ import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Zynd Protocol",
+  description: "The Inter-Agent Search Protocol for AI Agents.",
+  openGraph: {
+    title: "Zynd Protocol",
+    description: "The Inter-Agent Search Protocol for AI Agents",
+    images: ["https://www.p3ai.network/spectronlabs.png"],
+    url: "https://www.p3ai.network/",
+    type: "website",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const GA_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>Zynd Protocol</title>
-        <meta
-          name="description"
-          content="The Inter-Agent Search Protocol for AI Agents."
-        />
-        <meta property="og:title" content="Zynd Protocol" />
-        <meta
-          property="og:description"
-          content="The Inter-Agent Search Protocol for AI Agents"
-        />
-        <meta
-          property="og:image"
-          content="https://www.p3ai.network/spectronlabs.png"
-        />
-        <meta property="og:url" content="https://www.p3ai.network/" />
-        <meta property="og:type" content="website" />
-      </head>
       <body className={clsx(inter.className, "antialiased")}>
         <Script
-          async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
         />
         <Script id="google-analytics-init" strategy="afterInteractive">
           {`
